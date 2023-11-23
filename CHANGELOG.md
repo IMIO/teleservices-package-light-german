f0d35fb4244b405a0ae4ae559ead4c1a879b4e03
Daniel Muyshond <daniel.muyshond@imio.be>
2023-11-23 15:07:19 +0100

    [MCITISVIA-22] Update indus stuff to rename _german

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,19 +1,10 @@
-teleservices-package changelog
-==============================
+# teleservices-package changelog
+
 :App involved: iA.Téléservices, Publik
 :Info: This is the changelog for the teleservices-package
 :Authors: Daniel Muyshond, Nicolas Hislaire, Nicolas Selva
 :License: GNU Affero General Public License v3 or later (AGPLv3+)
 
-## [0.0.2] - 21-03-2023 -
+## [0.0.1] - 2023-11-23
 
-### Updated
-
-  - Version increment to try to fix the docker image not with latest version [dmshd]
-
-## [0.0.2] - 21-04-2022 -
-### Updated
-  - [TELE-1134] update changelog, include .wcs file via MANIFEST.in [dmuyshond]
-
-### Added
-  - 'pays' datasources which is a dependency of this package [dmshd]
+Init german fork of teleservices-package-light (teleservices-package-ligh-german)
--- setup.py
+++ setup.py
@@ -14,20 +14,20 @@ class inst(install):
             os.getcwd().replace(" ", r"\ ").replace("(", r"\(").replace(")", r"\)")
             + "/bin/"
         )
-        os.system("sh " + path + "install_teleservices_package_light.sh")
+        os.system("sh " + path + "install_teleservices_package_light_german.sh")
 
 
 version = "0.0.3"
 
 setup(
-    name="teleservices-package-light",
+    name="teleservices-package-light-german",
     version=version,
-    author="Daniel Muyshond, Nicolas Selva, Nicolas Hislaire",
+    author="iMio",
     author_email="support-ts@imio.be",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
-    url="https://github.com/IMIO//teleservices-package-light",
+    url="https://github.com/IMIO//teleservices-package-light-german",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
